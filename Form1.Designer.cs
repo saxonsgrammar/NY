83b4d59737e46986049ae6a140fb226e1132dbe1
saxonsgrammar <gramatikvlad1986@gmail.com>
2023-06-30 18:01:35 +0300

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace NY
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelTime = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelTime
+            // 
+            this.labelTime.AutoSize = true;
+            this.labelTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelTime.Location = new System.Drawing.Point(16, 11);
+            this.labelTime.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(0, 52);
+            this.labelTime.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(464, 100);
+            this.Controls.Add(this.labelTime);
+            this.Name = "Form1";
+            this.Text = "NY";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTime;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace NY
+{
+    public partial class Form1 : Form
+    {
+        private static Timer vTimer = new Timer();
+        public Form1()
+        {
+            InitializeComponent();
+            vTimer.Tick += new EventHandler(ShowTime);
+            vTimer.Interval = 30;
+            vTimer.Start();
+        }
+        private void ShowTime(object vObj, EventArgs e)
+        {
+            labelTime.Text = Convert.ToString((new DateTime(2024, 1, 1) - DateTime.Now).TotalSeconds);
+        }
+    }
+}
